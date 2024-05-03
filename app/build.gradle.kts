dfcd3e41502cef760b9bf2d4ffdf09a7db1f1b06
RanchMonster <Ranchmonster06@outlook.com>
2024-05-03 23:03:26 +0000

    May 3, 2024, 1:03 PM

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
-    id 'com.github.johnrengelman.shadow' version '8.1.1'
-     id 'java'
+    kotlin("jvm") version "1.5.21"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 repositories {
