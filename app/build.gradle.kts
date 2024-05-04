258d36442d79aad76dbd0e97546fbcfd84b44ea7
RanchMonster <Ranchmonster06@outlook.com>
2024-05-04 20:34:12 +0000

    May 4, 2024, 10:34 AM

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ application {
 tasks.getByName("shadowJar") {
     // Configure the shadow jar task as needed
     // For example, change the name of the output jar:
-    archiveBaseName.set('shadow')
+    archiveBaseName.set('shadow")
     archiveClassifier.set('')
 }
 tasks.withType<Jar> {
