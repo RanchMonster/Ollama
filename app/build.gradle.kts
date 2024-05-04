b8cb679f3fdecf8c8be7486e6f3b798f8482d3ca
RanchMonster <Ranchmonster06@outlook.com>
2024-05-04 04:37:48 +0000

    May 3, 2024, 6:37 PM

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,6 @@ application {
 //         configurations.compileClasspath.collect() { it.isDirectory() ? it : zipTree(it) }
 //     }
 // }
-archiveBaseName.set("my-application")
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
