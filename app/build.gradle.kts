db613181599b0c2882772adeef15824d87dde1ed
RanchMonster <Ranchmonster06@outlook.com>
2024-05-04 21:46:51 +0000

    May 4, 2024, 11:46 AM

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,6 @@ tasks.shadowJar {
     // For example, change the name of the output jar:
     archiveBaseName.set("App")
     archiveClassifier.set("")
-    useJUnitPlatform()
     // If you want to exclude any dependencies, you can do it like this:
     // dependencies {
     //     exclude("group:name")
