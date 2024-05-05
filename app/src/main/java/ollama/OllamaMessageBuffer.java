f580a2e55a8a5f235ee15d6122ebb3af0d2c2a53
RanchMonster <Ranchmonster06@outlook.com>
2024-05-05 01:32:05 +0000

    May 4, 2024, 3:32 PM

--- app/src/main/java/ollama/OllamaMessageBuffer.java
+++ app/src/main/java/ollama/OllamaMessageBuffer.java
@@ -1,7 +1,2 @@
 package ollama;
-public class OllamaMessageBuffer extends OllamaMessage {
-    public OllamaMessageBuffer(String content, String role){
-        super(content, role);
-    }
-    
-}
+
