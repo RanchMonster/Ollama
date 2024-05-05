22f9887ebba5b4a1f2334ff9cf570af15436b582
RanchMonster <Ranchmonster06@outlook.com>
2024-05-05 01:04:50 +0000

    May 4, 2024, 3:04 PM

--- app/src/main/java/ollama/App.java
+++ app/src/main/java/ollama/App.java
@@ -8,7 +8,7 @@ public class App {
         public static void main(String[] args) throws ResponseError {
         try {
             Ollama AI =Ollama.loadDefaultHost();
-            System.out.println(AI.chat("", null, false, null, null, null));
+            System.out.println(AI.chat("llama3", null, false, null, null, null));
         } catch (RequestError | CoroutineError | Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
