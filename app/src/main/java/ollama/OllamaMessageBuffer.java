0d9a51bc4190df2468b133b259c51c0e86b210b0
RanchMonster <Ranchmonster06@outlook.com>
2024-05-05 21:12:44 +0000

    May 5, 2024, 11:12 AM

--- app/src/main/java/ollama/OllamaMessageBuffer.java
+++ app/src/main/java/ollama/OllamaMessageBuffer.java
@@ -5,21 +5,14 @@ import java.io.Reader;
 import java.util.Iterator;
 import org.json.JSONObject;
 import org.json.JSONException;
-class OllamaMessageBuffer extends BufferedReader implements Iterable<OllamaMessage> {
+class OllamaMessageBuffer extends BufferedReader{
+    private OllamaMessageList messages;
     public OllamaMessageBuffer(Reader in) {
         super(in);
     }
     public OllamaMessageBuffer(Reader in, int sz) {
         super(in, sz);
     }
-    public Iterator <OllamaMessage> iterator() {
-        return new Iterator<OllamaMessage>() {
-            @Override
-
-            public boolean hasNext() {
-                // TODO Auto-generated method stub
-                return false;
-            }
-        };
+    
 }
 
