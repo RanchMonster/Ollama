624b4a02edd6a1fcee1e8d7d89acc3101a156500
RanchMonster <Ranchmonster06@outlook.com>
2024-05-05 23:11:52 +0000

    May 5, 2024, 1:11 PM

--- app/src/main/java/ollama/IterableFuture.java
+++ app/src/main/java/ollama/IterableFuture.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class IterableFuture<T> implements Iterable<T> {
     private ArrayList<BetterFuture<T>> futures;
     /**
-     * Iterable Future is used for Futures that you want to 
+     * Iterable Future is used for when you have many Better Futures you want to go through 
      */
     public IterableFuture(){
         this.futures= new ArrayList<BetterFuture<T>>();
