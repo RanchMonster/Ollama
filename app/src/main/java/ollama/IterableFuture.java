528339bc9518d139da7493239c15e90bda9dc6d6
RanchMonster <Ranchmonster06@outlook.com>
2024-05-05 23:11:04 +0000

    May 5, 2024, 1:11 PM

--- app/src/main/java/ollama/IterableFuture.java
+++ app/src/main/java/ollama/IterableFuture.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class IterableFuture<T> implements Iterable<T> {
     private ArrayList<BetterFuture<T>> futures;
     /**
-     * 
+     * Iterable Future
      */
     public IterableFuture(){
         this.futures= new ArrayList<BetterFuture<T>>();
