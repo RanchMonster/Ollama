26442423aae993f19f16447df4436c8797ad0eb2
RanchMonster <Ranchmonster06@outlook.com>
2024-05-05 21:11:12 +0000

    May 5, 2024, 11:11 AM

--- app/src/main/java/ollama/OllamaMessageBuffer.java
+++ app/src/main/java/ollama/OllamaMessageBuffer.java
@@ -1,46 +1,25 @@
 package ollama;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.Iterator;
 import org.json.JSONObject;
 import org.json.JSONException;
-class OllamaMessageBuffer implements Iterable<OllamaMessage> {
-    private Iterator<String> reader;
-    OllamaMessageBuffer(Iterator<String> reader) {
-        this.reader = reader;
-        while (!reader.hasNext()) {
-            continue;
-        }
+class OllamaMessageBuffer extends BufferedReader implements Iterable<OllamaMessage> {
+    public OllamaMessageBuffer(Reader in) {
+        super(in);
     }
-    public Iterator<OllamaMessage> iterator() {
+    public OllamaMessageBuffer(Reader in, int sz) {
+        super(in, sz);
+    }
+    public Iterator <OllamaMessage> iterator() {
         return new Iterator<OllamaMessage>() {
+            @Override
+
             public boolean hasNext() {
-                return reader.hasNext();
-            }
-            public OllamaMessage next() {
-                JSONObject obj = new JSONObject(reader.next());
-                OllamaMessage message=new OllamaMessage(obj.getString("content"), obj.getString("role"), obj.getBoolean("done"));
-                return message;
+                // TODO Auto-generated method stub
+                return false;
             }
         };
-    }
-    public OllamaMessage nostream() throws IOException{
-        String curr;
-        JSONObject obj;
-        OllamaMessageList messages=new OllamaMessageList();
-            while ((curr = reader.next())!=null) {
-                try{
-                    obj = new JSONObject(curr);
-                    messages.addMessage(new OllamaMessage(obj.getString("content"), obj.getString("role"), obj.getBoolean("done")));
-                }catch(JSONException e){
-                    continue;
-                }
-            }
-            OllamaMessage Message=messages.getMessage(0);
-            for (int x = 1; x < messages.size(); x++) {
-                Message.mergeChunck(messages.getMessage(x));
-            }
-            return Message;
-    }
 }
 
