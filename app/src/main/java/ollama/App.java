2bb615419ff9a8f2aab98603acbeb7072c370045
RanchMonster <Ranchmonster06@outlook.com>
2024-05-05 23:12:48 +0000

    May 5, 2024, 1:12 PM

--- app/src/main/java/ollama/App.java
+++ app/src/main/java/ollama/App.java
@@ -11,9 +11,6 @@ public class App {
             OllamaMessageList messages = new OllamaMessageList();
             messages.addMessage(new OllamaMessage("hello what can you do", OllamaMessage.USER));
             OllamaMessage message = AI.chat("llama3",messages ,true, null, null, null).await();
-            while (message!=null) {
-                System.out.println(message);
-            }
             System.out.println(AI.chat("llama3",messages ,true, null, null, null));
         } catch (RequestError | CoroutineError | Exception e) {
             // TODO Auto-generated catch block
