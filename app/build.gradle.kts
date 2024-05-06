95c8446f855d54f2647897f1d841118a03d8376b
RanchMonster <Ranchmonster06@outlook.com>
2024-05-06 20:47:37 +0000

    May 6, 2024, 10:47 AM

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,73 +1,61 @@
 plugins {
-    // Apply the application plugin to add support for building a CLI application in Java.
     application
-    // kotlin("jvm") version "1.5.21"
     id("com.github.johnrengelman.shadow") version "8.0.0"
     id("java")
     id("maven-publish")
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
 dependencies {
-    // Use JUnit Jupiter for testing.
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
     implementation("org.json:json:20140107")
     shadow("junit:junit:3.8.2")
-    // maven("maven-publish")
-    // This dependency is used by the application.
     implementation("com.google.guava:guava:31.1-jre")
 }
+
 publishing {
     publications {
         create<MavenPublication>("gpr") {
-            run {
-                groupId = "com.enefce.libraries"
-                artifactId = getArtificatId()
-                version = getVersionName()
-                artifact("$buildDir/outputs/aar/${getArtificatId()}-release.aar")
-            }
+            from(components["java"])
+            groupId = "com.enefce.libraries"
+            artifactId = "Ollama" // Set your artifactId
+            version = "1.0.0" // Set your version
+            artifact(tasks.shadowJar.get().archiveFile)
         }
     }
 }
-repositories{
+
+repositories {
     maven {
-      name = "GitHubPackages"
-      url = uri("https://maven.pkg.github.com/RanchMonster/Ollama")
-      credentials {
-            username = System.getenv("GITHUB_ACTOR")
-            password = System.getenv("GITHUB_TOKEN")
-            }
+        name = "GitHubPackages"
+        url = uri("https://maven.pkg.github.com/RanchMonster/Ollama")
+        credentials {
+            username = "RanchMonster"
+            password = "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"
+        }
     }
 }
+
 application {
-    // Define the main class for the application.
     mainClass.set("ollama.Ollama")
 }
 
 tasks.shadowJar {
-    // Configure the shadow jar task as needed
-    // For example, change the name of the output jar:
     archiveBaseName.set("Ollama")
     archiveClassifier.set("")
-    // If you want to exclude any dependencies, you can do it like this:
-    // dependencies {
-    //     exclude("group:name")
-    // }
 }
 
 tasks.named<Jar>("jar") {
-    // Change the name of the regular JAR file
-    enabled= false
+    enabled = false
 }
 
 tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 }
+
 tasks.named("distZip") {
     dependsOn(":App:shadowJar")
 }
