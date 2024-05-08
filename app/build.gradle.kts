8cded1f3634bfa239d2e899f5542d68bebb5fe79
RanchMonster <Ranchmonster06@outlook.com>
2024-05-08 02:20:47 +0000

    May 7, 2024, 4:20 PM

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ task.writeNewPom {
                 licenses {
                     license {
                         name="The Apache Software License, Version 2.0"
-                        url "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                        url ="http://www.apache.org/licenses/LICENSE-2.0.txt"
                         distribution "repo"
                     }
                 }
