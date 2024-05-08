a75f90cee2c05106c1835da25669501755680af6
RanchMonster <Ranchmonster06@outlook.com>
2024-05-08 05:22:40 +0000

    May 7, 2024, 7:22 PM

--- app/src/main/java/ollama/IterableFuture.java
+++ app/src/main/java/ollama/IterableFuture.java
@@ -37,7 +37,7 @@ public class IterableFuture<T> implements Iterable<T> {
                     return true;
                 }else{
                     try {
-                        Thread.sleep(1000);
+                        Thread.sleep(100);
                     } catch (InterruptedException e) {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
