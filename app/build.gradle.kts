fc1063580d005d4537a5ecd962a6c9b5e3d613ee
RanchMonster <Ranchmonster06@outlook.com>
2024-05-08 02:20:15 +0000

    May 7, 2024, 4:20 PM

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ task.writeNewPom {
     doLast {
         pom {
             project {
-                groupId ="org.example'
+                groupId ="org.example"
                 artifactId 'test'
                 version '1.0.0'
                 inceptionYear '2008'
