704cd5f893922a8f338f65aba63c76dd7cbab297
RanchMonster <Ranchmonster06@outlook.com>
2024-05-14 06:42:55 +0000

    May 13, 2024, 8:42 PM

--- app/src/main/java/ollama/App.java
+++ app/src/main/java/ollama/App.java
@@ -6,7 +6,7 @@ package ollama;
 
 public class App {
         public static void main(String[] args) throws ResponseError {
-            System.out.println("ran ollama");
+            System.out.println("R");
     }
 }
 
